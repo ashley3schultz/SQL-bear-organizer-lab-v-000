0d4753e2a73866621c5fc83e4d320bd0f4167467
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-31 03:02:09 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,7 +2,7 @@ CREATE TABLE bears (
   id INTEGER PRIMMARY KEY,
   name TEXT,
   age INTEGER,
-  gender TEXT NOT NULL CHECK (gender IN ('M', 'F'))
+  gender TEXT NOT NULL CHECK (gender IN ('M', 'F')),
   color TEXT,
   temperament TEXT,
   alive ALIVE BOOLEAN DEFAULT 1
